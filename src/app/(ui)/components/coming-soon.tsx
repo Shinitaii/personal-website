2b95f5345e8851e336a5d9a04fd6c35035af115a
Shinn <sunikhedge@gmail.com>
2024-06-07 06:26:53 +0800

    added coming soon page.

---
+++ src/app/(ui)/components/coming-soon.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon(){
+    return (
+        <main className="w-full h-screen flex justify-center items-center">
+            <h1 className='text-6xl'>Coming Soon.</h1>
+        </main>
+    )
+}
