59dca1e99f3be5e77268a5f23d7a0f5acc3a39fd
Shinn <sunikhedge@gmail.com>
2024-06-07 08:56:10 +0800

    moved page content in a different branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 import Link from "next/link";
-import ComingSoon from "./(ui)/components/coming-soon";
 
 export default function Home() {
   return ( 
-    <ComingSoon/>
+    <main className="w-full h-screen flex flex-col justify-center items-center sm:text-sm md:text-lg lg:text-xl">
+      <div className="w-1/2 flex justify-evenly items-end">
+        <Link href='#about'>About</Link>
+        <div>
+          <h1 className='flex justify-center'>Richmond Glenn Viloria</h1>
+          <h3 className='flex justify-center'>An aspiring software engineer.</h3>
+        </div>
+        <Link href='#about'>Projects</Link>
+      </div>
+      <div className='flex justify-evenly w-1/2'>
+        <a href='https://github.com/Shinitaii'>Github</a>
+        <a href='https://www.linkedin.com/in/richmond-glenn-viloria/'>LinkedIn</a>
+        <a href='mailto:rgviloria1@gmail.com'>Email</a>
+      </div>
+    </main>
   );
 }
